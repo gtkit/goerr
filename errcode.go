43cd5a05f727d2bf7e2cb397a544ef6eeed7a505
xiaozhaofu <xiaozhaofu@163.com>
2023-07-27 20:53:32 +0800

    优化

--- errcode.go
+++ errcode.go
@@ -42,121 +42,168 @@ type ErrCode struct {
 }
 
 var (
+	// ErrNo 请求成功
 	ErrNo = ErrCode{
 		Code:     10010000,
 		HTTPCode: 200,
 		Desc:     "OK",
 	}
-	ErrRequestFail = ErrCode{
-		Code:     101,
-		HTTPCode: 200,
-		Desc:     "请求失败",
-	}
+
+	// ErrInternalServer 服务器内部错误
 	ErrInternalServer = ErrCode{
 		Code:     10010001,
 		HTTPCode: 500,
 		Desc:     "Internal server error",
 	}
+
+	// ErrParams 非法参数
 	ErrParams = ErrCode{
 		Code:     10010002,
 		HTTPCode: 422,
 		Desc:     "Illegal params",
 	}
-	ErrValidateParams = ErrCode{
-		Code:     10010012,
-		HTTPCode: 422,
-		Desc:     "validator.ValidationErrors",
-	}
+
+	// ErrAuthentication 身份验证失败
 	ErrAuthentication = ErrCode{
 		Code:     10010003,
 		HTTPCode: 401,
 		Desc:     "Authentication failed",
 	}
+
+	// ErrNotFound 找不到
 	ErrNotFound = ErrCode{
 		Code:     10010004,
 		HTTPCode: 404,
 		Desc:     "Page not found",
 	}
+
+	// ErrAuthenticationHeader 认证头非法
 	ErrAuthenticationHeader = ErrCode{
 		Code:     10010005,
 		HTTPCode: 403,
 		Desc:     "Authentication header Illegal",
 	}
+
+	// ErrAppKey 无效应用程序密钥
 	ErrAppKey = ErrCode{
 		Code:     10010006,
 		HTTPCode: 403,
 		Desc:     "Invalid app key",
 	}
+
+	// ErrSign 无效应用签名
 	ErrSign = ErrCode{
 		Code:     10010007,
 		HTTPCode: 401,
 		Desc:     "Invalid secret key",
 	}
+
+	// ErrPermission 没有权限
 	ErrPermission = ErrCode{
 		Code:     10010008,
 		HTTPCode: 403,
 		Desc:     "Permission denied",
 	}
-	ErrTooManyRequests = ErrCode{
-		Code:     10010013,
-		HTTPCode: 429,
-		Desc:     "Too Many Requests",
-	}
+
+	// ErrInvalidJson 无效的JSON
 	ErrInvalidJson = ErrCode{
 		Code:     10010009,
 		HTTPCode: 500,
 		Desc:     "Invalid Json",
 	}
+
+	// ErrTimeout 服务器响应超时
 	ErrTimeout = ErrCode{
 		Code:     10010010,
 		HTTPCode: 408,
 		Desc:     "Server response timeout",
 	}
+
+	// ErrAuthExpired Authentication expired
 	ErrAuthExpired = ErrCode{
 		Code:     10010011,
 		HTTPCode: 504,
 		Desc:     "Authentication expired",
 	}
+
+	// ErrValidateParams 验证器。验证错误
+	ErrValidateParams = ErrCode{
+		Code:     10010012,
+		HTTPCode: 422,
+		Desc:     "validator.ValidationErrors",
+	}
+
+	// ErrTooManyRequests 请求过多
+	ErrTooManyRequests = ErrCode{
+		Code:     10010013,
+		HTTPCode: 429,
+		Desc:     "Too Many Requests",
+	}
+
+	// ErrRequestFail 请求失败或者响应为空
+	ErrRequestFail = ErrCode{
+		Code:     10010014,
+		HTTPCode: 200,
+		Desc:     "请求失败",
+	}
+
+	// ErrElasticsearchServer Elasticsearch 服务器错误
 	ErrElasticsearchServer = ErrCode{
 		Code:     10010101,
 		HTTPCode: 500,
 		Desc:     "Elasticsearch server error",
 	}
+
+	// ErrElasticsearchDSL Elasticsearch DSL 错误
 	ErrElasticsearchDSL = ErrCode{
 		Code:     10010102,
 		HTTPCode: 500,
 		Desc:     "Elasticsearch  DSL error",
 	}
+
+	// ErrMysqlServer Mysql 服务器错误
 	ErrMysqlServer = ErrCode{
 		Code:     10010201,
 		HTTPCode: 500,
 		Desc:     "Mysql server error",
 	}
+
+	// ErrMysqlSQL 非法SQL
 	ErrMysqlSQL = ErrCode{
 		Code:     10010202,
 		HTTPCode: 500,
 		Desc:     "Illegal SQL",
 	}
+
+	// ErrMongoServer MongoDB 服务器错误
 	ErrMongoServer = ErrCode{
 		Code:     10010301,
 		HTTPCode: 500,
 		Desc:     "MongoDB server error",
 	}
+
+	// ErrMongoDSL MongoDB DSL 错误
 	ErrMongoDSL = ErrCode{
 		Code:     10010302,
 		HTTPCode: 500,
 		Desc:     "MongoDB DSL error",
 	}
+
+	// ErrRedisServer Redis 服务器错误
 	ErrRedisServer = ErrCode{
 		Code:     10010401,
 		HTTPCode: 500,
 		Desc:     "Redis server error",
 	}
+
+	// ErrKafkaServer Kafka 服务器错误
 	ErrKafkaServer = ErrCode{
 		Code:     10010501,
 		HTTPCode: 500,
 		Desc:     "Kafka server error",
 	}
+
+	// ErrRabbitMQServer RabbitMq 服务器错误
 	ErrRabbitMQServer = ErrCode{
 		Code:     10010601,
 		HTTPCode: 500,
